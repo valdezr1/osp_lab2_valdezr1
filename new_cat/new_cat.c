795635b9324586cf28a4970fcc1c6e74f2a8ca11
Valdez <valdezr1@debian-9.3.0-template>
2018-04-17 14:45:46 -0700

    Added no argument echos

--- new_cat/new_cat.c
+++ new_cat/new_cat.c
@@ -1,37 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <signal.h>
 
 int main(int argc, char** argv){
 	const int MAX = 1000; //to initialize char array. Max to 1000 char per line
 	FILE* fp; //instance of file
 	char str[MAX];//char array
+	char* buff;
+	int read = 0;
+	size_t nbytes = 1000;
 
-	if(argc != 2){
+	if(argc > 2){
 		//argv[0] = programName
 		//arg[1] = readFile
-		//if argc is not 2, then there are too many
-		//	arguments or not enough.
+		//if argc is greater than 2, then there are too many
 		
 		printf("Invalid argument provided\n");
 		exit(EXIT_FAILURE);
 
 	}
+	else if(argc == 2){ //if it is 2 then print the file, only accept one file
 
-	fp = fopen(argv[1], "r"); //read the file (if it exists)
+		fp = fopen(argv[1], "r"); //read the file (if it exists)
 
-	//File not found or does not exist
-	if(fp == NULL){
-		printf("File not found\n");
-		exit(EXIT_FAILURE);
+		//File not found or does not exist
+		if(fp == NULL){
+			printf("File not found\n");
+			exit(EXIT_FAILURE);
+		}
+
+		//keep reading file and print untill null (eof)
+		while(fgets(str, MAX, fp) != NULL){
+			printf("%s", str);
+		}
+
+		//close file
+		fclose(fp);
 	}
+	else{
+		//argc == 1 where only the program is ran but no other args
+		
+		buff = (char*)malloc(nbytes*sizeof(str));
+		
+		while(1){
+			read = getline(&buff, &nbytes, stdin);
+			if(read == -1){
+				break;
+			}
+			printf(buff);
+		}
 
-	//keep reading file and print untill null (eof)
-	while(fgets(str, MAX, fp) != NULL){
-		printf("%s", str);
+		free(buff);
+
 	}
-
-	//close file
-	fclose(fp);
 
 
 	return 0;
